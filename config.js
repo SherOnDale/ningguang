40d861d4ecc6712adade45def0d76a0123333840
Sherin Binu <sherinbinu@hotmail.com>
2021-01-25 16:51:56 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+if(process.env.NODE_ENV === 'production') {
+    require('dotenv').config()
+}
+
+module.exports = {
+    botToken: process.env.BOT_TOKEN || '',
+    prefix: process.env.PREFIX || 'n!',
+    mongoUrl: process.env.MONGO_URL || '',
+    ownerId: '361985924571332618',
+    listenChannels: ['574305553895915529'],
+    retardedId: '361985924571332618'
+}
