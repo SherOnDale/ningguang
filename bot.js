8de1e0d4634488b758666747e8a29ebd6c01a19c
Sherin Binu <sherinbinu@hotmail.com>
2021-01-25 17:03:53 +0530

    check changes

--- bot.js
+++ bot.js
@@ -4,3 +4,7 @@ const config = require('./config')
 const client = new Discord.Client()
 
 client.login(config.botToken)
+client.on("ready", function (evt) {
+  console.log("Connected");
+  console.log(`Logged in as ${client.user.tag}`);
+});
