4dc14034d2f995573709d2a1a9d3e7b2d8dbab2b
Sherin Binu <sherinbinu@hotmail.com>
2021-01-26 18:41:15 +0530

    shuffle entries

--- bot.js
+++ bot.js
@@ -19,6 +19,25 @@ client.on("ready", function (evt) {
   });
 });
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 client.on('message', async function (message) {
     if(message.author.bot) return
 
@@ -113,6 +132,7 @@ client.on('message', async function (message) {
                   entries.push(result[i].id)
                 }
               }
+              shuffle(entries)
               const winner = Math.floor(Math.random() * entries.length);
               client.channels.cache.get(message.channel.id).send('(Testing) Congrats <@' + entries[winner] + '>, You won!')
             })
